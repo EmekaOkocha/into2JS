b6edd5d3f1496e442f763d31551197177218c8f7
Chukwuemeka <okocha007@gmail.com>
2022-06-24 10:43:06 +0100

    prints out name hight and country

---
+++ intro.js
@@ -0,0 +1,12 @@
+let myName = "Chukwuemeka Okocha";
+let height = "1.78m";
+let country = "Nigeria";
+
+//function myMethod()
+//{
+    console.log("My name is: " +" "+ myName);
+    console.log("My name height is: " +" "+ height);
+    console.log(country + " "+"is my country");
+//}
+// myMethod();
+
